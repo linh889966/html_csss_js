6a10724770043fbfead0dad49facc4f02e2184d0
Pandora617 <linh41997@gmail.com>
2025-03-14 08:19:13 +0700

    :abc

--- session27/bt5.js
+++ session27/bt5.js
@@ -1,16 +1,17 @@
-function bai5(arr, n) {
-    let result = [];
-    if(Array.isArray(arr)){
-        for (let i = 0; i < arr.length; i += n) {
-            result.push(arr.slice(i, i + n));
-        }
-    
-        return result;
-    } else {
-        console.log("du lieu khong hop le");
-        return;
+function mang(arr, n) {
+    if (!arr || !arr.slice || !arr.length || !n || n <= 0) {
+        return "dữ liệu không hợp lệ";
     }
+
+    let result = [];
+    for (let i = 0; i < arr.length; i += n) {
+        result.push(arr.slice(i, i + n)); 
+    }
+    return result;
 }
 
-console.log(bai5([1, 2, 3, 4, 5, 6, 7, 8], 3));
-console.log(bai5("anh",3));
+console.log(mang([1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(mang([1, 2, 3, 4, 5, 6, 7, 8], 2));
+console.log(mang("abc", 3)); 
+console.log(mang(12345, 3)); 
+console.log(mang([], 2));
